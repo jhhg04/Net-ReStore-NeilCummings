fe006d972426c2b60d5425e9db6493f76d4bfa4d
jhhg04 <jhhg04@gmail.com>
2023-11-15 22:48:13 -0500

    End of section 5

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Product } from '../../app/models/product';
@@ -12,6 +11,9 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
+import agent from '../../app/api/agent';
+import NotFound from '../../app/errors/NotFound';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -19,16 +21,19 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/products/${id}`)
-      .then((res) => setProduct(res.data))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+    // axios
+    //   .get(`http://localhost:5000/api/products/${id}`)
+    id &&
+      agent.Catalog.details(parseInt(id))
+        // .then((res) => setProduct(res.data))
+        .then((response) => setProduct(response))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <LoadingComponent message='Loading Product...' />;
 
-  if (!product) return <h3>Product Not Found</h3>;
+  if (!product) return <NotFound />;
 
   return (
     <Grid container spacing={6}>
